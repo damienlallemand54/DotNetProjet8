7cb02baac86438f58c0d573445511c727c3e553d
Damien Lallemand <damienlallemand54@gmail.com>
2026-05-08 12:45:51 +0200

    Ajout du SKIP après tests

--- TourGuideTest/PerformanceTest.cs
+++ TourGuideTest/PerformanceTest.cs
@@ -68,7 +68,7 @@ namespace TourGuideTest
             Assert.True(TimeSpan.FromMinutes(15).TotalSeconds >= stopWatch.Elapsed.TotalSeconds);
         }
 
-        [Fact]
+        [Fact(Skip = "Delete Skip when you want to pass the test")]
         public void HighVolumeGetRewards()
         {
             //On peut ici augmenter le nombre d'utilisateurs pour tester les performances
